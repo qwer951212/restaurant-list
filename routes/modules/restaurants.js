4af36e1c2ef85285caa7ad153f6e4a66a502dd39
yiping <qwer951212@gmail.com>
2021-02-02 16:53:33 +0800

    fix: fix delete confirm message

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -2,8 +2,6 @@ const express = require('express')
 const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
-
-
 router.get('/new', (req, res) => {
   return res.render('new')
 })
